dba27bf26de4e5c21e8cf4f97cc7eaf32e567df7
gsriley <gsriley@protonmail.com>
2026-03-11 18:26:46 +0200

    Initial clean commit

---
+++ C# dummy web server/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Text;
+
+namespace DummyServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost:8080/");
+            listener.Start();
+
+            Console.WriteLine("Server running at http://localhost:8080/");
+
+            while (true)
+            {
+                HttpListenerContext context = listener.GetContext();
+                HttpListenerResponse response = context.Response;
+
+                string responseString = "<html><body><h1>Hello from C# server</h1></body></html>";
+                byte[] buffer = Encoding.UTF8.GetBytes(responseString);
+
+                response.ContentLength64 = buffer.Length;
+                response.ContentType = "text/html";
+
+                response.OutputStream.Write(buffer, 0, buffer.Length);
+                response.OutputStream.Close();
+            }
+        }
+    }
+}
